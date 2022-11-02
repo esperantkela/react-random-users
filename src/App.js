efe6d84221e060c7ea7dd7f01961932bd7739817
esperantkela <esperant.kela@gmail.com>
2022-11-02 10:44:46 +0100

    float button

--- src/App.js
+++ src/App.js
@@ -5,9 +5,9 @@ function App() {
   const  [count, setCount] = useState(1);
   return (
     <div className="container">
-      <h1 className='text-center'>Usememo</h1>
-        <button onClick={() => setCount(count + 1)} className='btn btn-info mb-3'>Increment {count}</button>
-        <button className='btn btn-dark mb-3 float-right'>changer</button>
+        <h1 className='text-center'>Usememo()</h1>
+        <button onClick={() => setCount(count + 1)} className='btn btn-primary mb-3'>Increment {count}</button>
+        <button className='btn btn-dark mb-3 float-end'>changer</button>
       <Profile/>
     </div>
   );

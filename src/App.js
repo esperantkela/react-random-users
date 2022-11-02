1c6b08fceb6ba179334358082d1fc9d1cb2605c9
esperantkela <esperant.kela@gmail.com>
2022-11-02 10:30:15 +0100

    profile

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
-import './App.css';
+import React , {useState} from 'react';
 import Profile from './components/Profile';
 
 function App() {
+  const  [count, setCount] = useState(1);
   return (
     <div className="container">
       <h1 className='text-center'>Usememo</h1>
-        <button className='btn btn-info mb-3'>Increment</button>
+        <button onClick={() => setCount(count + 1)} className='btn btn-info mb-3'>Increment {count}</button>
         <button className='btn btn-dark mb-3 float-right'>changer</button>
       <Profile/>
     </div>
--- src/components/Profile.js
+++ src/components/Profile.js
@@ -2,7 +2,14 @@ import React from 'react'
 
 function Profile() {
   return (
-    <div>Profile</div>
+    <div>
+      <ul className="list-group">
+        <li className='list-group-item'><p className='h2'>User Id:</p></li>
+        <li className='list-group-item'><strong>Nom : </strong></li>
+        <li className='list-group-item'><strong>pseudo : </strong></li>
+        <li className='list-group-item'><strong>Email </strong></li>
+      </ul>
+    </div>
   )
 }
 

44b00286a59869174fb28dc7f3e0a991f5af6ab3
esperantkela <esperant.kela@gmail.com>
2022-11-02 13:02:37 +0100

    refactor code

--- src/App.js
+++ src/App.js
@@ -1,19 +1,49 @@
-import React , {useState, useEffect} from 'react';
+import React , {useState, useEffect, useMemo} from 'react';
 import Profile from './components/Profile';
+import axios from 'axios'
 
 function App() {
   const  [count, setCount] = useState(1);
+  const  [profile, setProfile] = useState({});
+  const  [dark, setDark] = useState(false);
 
   useEffect(() => {
-   
-  });
+   axios
+      .get(`https://jsonplaceholder.typicode.com/users/${count}`)
+      .then(response=>{
+        setProfile(response.data)
+      })
+      .catch(error =>{
+        console.log(error)
+      })
+  }, [count]);
+
+  const memoiseValue = useMemo(()=>{
+    console.log('je suis dans isOverTen')
+    return count > 10
+  }, [count]);
+
+
+  const goDark = ()=>{
+    setDark(!dark)
+
+    if(!dark){
+      document.body.classList.add("bg-secondary")
+    }else{
+      document.body.classList.remove("bg-secondary")
+    }
+  }
+  const btnTheme = dark ? 'btn-light' : 'btn-dark'
+  const textBtnTheme = dark ? 'rendre claire' : 'rendre sombre'
 
   return (
+    
     <div className="container">
-        <h1 className='text-center'>Usememo()</h1>
+        <h1 className='text-center'>User random()</h1>
+        {memoiseValue && <div className='alert alert-danger' role='alert'>STOP !!</div>}
         <button onClick={() => setCount(count + 1)} className='btn btn-primary mb-3'>Increment {count}</button>
-        <button className='btn btn-dark mb-3 float-end'>changer</button>
-      <Profile/>
+        <button className={`btn ${btnTheme} mb-3 float-end`} onClick={goDark}>{textBtnTheme}</button>
+      <Profile count={count} profile={profile}/>
     </div>
   );
 }
--- src/components/Profile.js
+++ src/components/Profile.js
@@ -1,13 +1,14 @@
 import React from 'react'
 
-function Profile() {
+function Profile({count, profile}) {
+console.log('je suis dans profile')
   return (
     <div>
       <ul className="list-group">
-        <li className='list-group-item'><p className='h2'>User Id:</p></li>
-        <li className='list-group-item'><strong>Nom : </strong></li>
-        <li className='list-group-item'><strong>pseudo : </strong></li>
-        <li className='list-group-item'><strong>Email </strong></li>
+        <li className='list-group-item'><p className='h2'>User Id: {count}</p></li>
+        <li className='list-group-item'><strong>Nom : {profile.name} </strong></li>
+        <li className='list-group-item'><strong>pseudo :  {profile.username}</strong></li>
+        <li className='list-group-item'><strong>Email :  {profile.email} </strong></li>
       </ul>
     </div>
   )
